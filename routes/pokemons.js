ee1d1a9f1f86b4b1550359d34951d6c3dbdfa233
Thiago Rodrigues <thiago.rodrigues365701@gmail.com>
2026-01-26 20:57:55 -0300

    refactor: update Pokémon routes to handle async operations and improve error handling

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const clientPromise = require("../banco-de-dados/config");
+const { ObjectId } = require("mongodb");
 
 router.get("/", async (req, res) => {
   try {
@@ -13,11 +14,45 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Erro ao buscar pokemons" });
   }
 });
-router.get("/:id", (req, res) => {
-  res.send(`pokemons de id ${req.params.id}`);
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  if (!ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "ID inválido" });
+  }
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("thiago_db");
+
+    const pokemon = await db
+      .collection("Pokemons")
+      .findOne({ _id: new ObjectId(id) });
+    if (!pokemon) {
+      return res.status(404).json({ error: "Pokémon não encontrado" });
+    }
+    res.json(pokemon);
+  } catch (err) {
+    console.error("Erro ao buscar pokémon:", err);
+    res.status(500).json({ error: "Erro ao buscar pokémon" });
+  }
 });
-router.post("/", (req, res) => {
-  const { texto } = req?.body;
-  res.json({ recebido: texto });
+
+router.post("/", async (req, res) => {
+  const novoPokemon = req.body;
+  try {
+    const client = await clientPromise;
+    const db = client.db("thiago_db");
+    const resultado = await db.collection("Pokemons").insertOne(novoPokemon);
+
+    res.status(201).json({
+      message: "Pokémon criado com sucesso",
+      id: resultado.insertedId,
+    });
+  } catch (err) {
+    console.error("Erro ao criar pokémon:", err);
+    res.status(500).json({ error: "Erro ao criar pokémon" });
+  }
 });
+
 module.exports = router;
